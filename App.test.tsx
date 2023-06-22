64e1afa60f0afcfd1983ebc7e866d5a7b87c388f
bluecat870 <alenzer0902@gmail.com>
2023-06-22 06:27:10 -0400

    add jest mock to skip

--- App.test.tsx
+++ App.test.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import App from './App';
 jest.mock('@react-native-async-storage/async-storage', () => require('@react-native-async-storage/async-storage/jest/async-storage-mock'));
 
-// beforeAll(() => { 
-//   jest.mock('@react-native-community/async-storage');
-// })
-describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree: any = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
+jest.mock('expo-linking', () => {
+  const module: typeof import('expo-linking') = {
+      ...jest.requireActual('expo-linking'),
+      createURL: jest.fn(),
+  };
+
+  return module;
 });
+jest.mock('@react-navigation/native');
+jest.mock('@react-navigation/bottom-tabs', () => {
+  const module: typeof import('@react-navigation/bottom-tabs') = {
+      ...jest.requireActual('@react-navigation/bottom-tabs'),
+      createBottomTabNavigator: jest.fn(),
+  };
+
+  return module;
+});
+
+import React from 'react';
+import {create, act} from 'react-test-renderer';
+
+import App from './App';
+
+
+beforeAll(() => {
+
+})
+
+describe('<App /> Basics', () => {
+  it('has 1 child', async () => {
+    let tree:any;
+    act(()=>{
+       tree = create(<App />)
+    })
+    expect(tree.toJSON().children.length).toBe(1);
+  });
+})
