3031fd3f5a10fed6dc3ad1bde2a4360b98fb965f
pcharlesme <opeyemicharlese@gmail.com>
2024-03-04 08:42:50 +0100

    Profile_Collection clear

--- src/components/SignUp/ProfilePicsCollection.tsx
+++ src/components/SignUp/ProfilePicsCollection.tsx
@@ -1,76 +1,66 @@
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  Pressable,
-  Dimensions,
-} from 'react-native';
-import React, { useState } from 'react';
-import { useFonts } from 'expo-font';
-import { images, fonts, appColor } from '../../constants';
-const { height, width } = Dimensions.get('window');
-import { useAppDispatch, useAppSelector } from '../../controller/hooks';
-import {
-  updateSelectedCollection,
-  updateSelectedRender,
-  updateNftRender,
-  updateNftOpen,
-  updateProfilePics,
-} from '../../controller/BottomSheetController';
-import { updateProfileImage } from '../../controller/UserController';
-import { sizes } from '../../utils';
-import { batch } from 'react-redux';
+import { View, Text, Image, Pressable, Dimensions } from "react-native";
+import { useFonts } from "expo-font";
+import { images, fonts, appColor } from "../../../constants";
+const { height, width } = Dimensions.get("window");
+import { useAppDispatch, useAppSelector } from "../../../controller/hooks";
+import { nftAssets, updateProfilePics } from "../../../controller/BottomSheetController";
+import { updateProfileImage } from "../../../controller/UserController";
+import { sizes } from "../../../utils";
+import { batch } from "react-redux";
+import { FlashList } from "@shopify/flash-list";
 const size = new sizes(height, width);
 const ProfilePicsCollection = () => {
   const profilePicture = useAppSelector(
     (state) => state.bottomSheetController.profilePics
   );
+  const assets  = useAppSelector(nftAssets)
+   
+  
   const dispatch = useAppDispatch();
   const profilePics = [
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#1",
       id: 1,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#2",
       id: 2,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#3",
       id: 3,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#4",
       id: 5,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#5",
       id: 6,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#6",
       id: 7,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#928098098",
       id: 8,
     },
     {
       image: images.Aptomingos,
-      Name: '#928098098',
+      Name: "#928098098",
       id: 9,
     },
   ];
   let [isLoaded] = useFonts({
-    'Outfit-Bold': fonts.OUTFIT_BOLD,
+    "Outfit-Bold": fonts.OUTFIT_BOLD,
   });
   if (!isLoaded) {
     return null;
@@ -79,62 +69,67 @@ const ProfilePicsCollection = () => {
     <View
       style={{
         flex: 1,
-        flexDirection: 'row',
-        flexWrap: 'wrap',
+        flexDirection: "row",
+        flexWrap: "wrap",
         width: size.getWidthSize(328),
-        justifyContent: 'space-between',
-        alignSelf: 'center',
+        justifyContent: "space-between",
+        alignSelf: "center",
         paddingHorizontal: size.getWidthSize(16),
       }}
     >
-      {profilePics.map((profile) => (
+      {assets?.map((profile) => (
         <Pressable
           style={{
             marginBottom: size.getHeightSize(16),
             borderWidth:
-              profilePicture.id && profile.id === profilePicture.id ? 8 : 0,
+              profilePicture.id && profile.asset_id === profilePicture.id
+                ? 8
+                : 0,
             borderRadius: 20,
             borderColor:
-              typeof profilePicture === 'undefined'
-                ? 'undefined'
+              typeof profilePicture === "undefined"
+                ? "undefined"
                 : appColor.kSecondaryButtonColor,
-            overflow: 'hidden',
+            overflow: "hidden",
             width: size.getWidthSize(140),
             height: size.getHeightSize(140),
           }}
           onPress={() => {
             batch(() => {
-              dispatch(updateProfilePics(profile));
               dispatch(
-                updateProfileImage(Image.resolveAssetSource(profile.image).uri)
+                updateProfilePics({
+                  image: profile.image_uri,
+                  name: profile.name,
+                  id: profile.asset_id,
+                })
               );
             });
           }}
-          key={profile.id}
+          key={profile.asset_id}
         >
           <Image
             style={{
               width: size.getWidthSize(140),
               height: size.getHeightSize(140),
             }}
-            source={profile.image}
+            source={{ uri: profile.image_uri }}
             resizeMode="cover"
           />
           <View
             style={{
               width: size.getWidthSize(124),
-              position: 'absolute',
-              backgroundColor: '#121212',
+              position: "absolute",
+              backgroundColor: "#121212",
               bottom:
-                profilePicture.id && profile.id === profilePicture.id
+                profilePicture.id && profile.asset_id === profilePicture.id
                   ? size.getHeightSize(0)
                   : size.getHeightSize(8),
               left:
-                profilePicture.id && profile.id === profilePicture.id
+                profilePicture.id && profile.asset_id === profilePicture.id
                   ? size.getWidthSize(0)
                   : size.getWidthSize(8),
               right: 0,
-              justifyContent: 'center',
+              justifyContent: "center",
               borderRadius: 8,
               paddingVertical: size.getHeightSize(8),
               paddingHorizontal: size.getWidthSize(10),
@@ -145,11 +140,11 @@ const ProfilePicsCollection = () => {
               style={{
                 color: appColor.kTextColor,
                 fontSize: size.fontSize(16),
-                fontFamily: 'Outfit-Regular',
+                fontFamily: "Outfit-Regular",
                 lineHeight: size.getHeightSize(18),
               }}
             >
-              {profile.Name}
+              {profile.name}
             </Text>
           </View>
         </Pressable>
